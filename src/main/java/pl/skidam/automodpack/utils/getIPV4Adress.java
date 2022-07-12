274cfea50804fc6b42335ba608ef1b3eb830d309
Skidam <skidamgamer@gmail.com>
2022-07-12 17:53:08 +0200

    config fix 2.1.3

--- src/main/java/pl/skidam/automodpack/utils/getIPV4Adress.java
+++ src/main/java/pl/skidam/automodpack/utils/getIPV4Adress.java
@@ -1,14 +1,20 @@
 package pl.skidam.automodpack.utils;
 
+import pl.skidam.automodpack.AutoModpackMain;
+
 import java.nio.charset.StandardCharsets;
 
 public class getIPV4Adress {
     public static String getIPV4Address() {
-        try (java.util.Scanner s = new java.util.Scanner(new java.net.URL("https://api.ipify.org").openStream(), StandardCharsets.UTF_8).useDelimiter("\\A")) {
-            return s.next();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
+        if (InternetConnectionCheck.InternetConnectionCheck()) {
+            try (java.util.Scanner s = new java.util.Scanner(new java.net.URL("https://api.ipify.org").openStream(), StandardCharsets.UTF_8).useDelimiter("\\A")) {
+                return s.next();
+            } catch (Exception e) {
+                AutoModpackMain.LOGGER.error("Failed to get IPV4 address!\n" + e);
+                return "";
+            }
+        } else {
+            return "";
         }
     }
 }
