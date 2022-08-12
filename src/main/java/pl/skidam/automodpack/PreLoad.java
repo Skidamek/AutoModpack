4819084d1cf87f8d9dc2ed5c4e8b6a4f890abd49
Skidam <skidamgamer@gmail.com>
2022-08-12 18:11:04 +0200

    disable it for now, fix it latter! #45

--- src/main/java/pl/skidam/automodpack/PreLoad.java
+++ src/main/java/pl/skidam/automodpack/PreLoad.java
@@ -52,7 +52,7 @@ public class PreLoad implements PreLaunchEntrypoint {
 
             new LoadModpackLink();
 
-            new CheckModpack(true);
+//            new CheckModpack(true);  // TODO fix it
 
             new DeleteTrashedMods();
 
