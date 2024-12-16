3d4655c0018ed4fb024328780f98ab65fa1bb646
suerion <55131959+suerion@users.noreply.github.com>
2024-12-16 22:50:38 +0100

    Trying to change UI to look how it work
    Added 2 Buttons and changed one 
    
    highend
    lowend
    complete server pack

--- src/main/java/pl/skidam/automodpack/client/ui/DangerScreen.java
+++ src/main/java/pl/skidam/automodpack/client/ui/DangerScreen.java
@@ -33,8 +33,16 @@ public class DangerScreen extends VersionedScreen {
             this.client.setScreen(parent);
         }));
 
-        this.addDrawableChild(buttonWidget(this.width / 2 + 15, this.height / 2 + 50, 120, 20, VersionedText.translatable("automodpack.danger.confirm").formatted(Formatting.BOLD), button -> {
-            Util.getMainWorkerExecutor().execute(modpackUpdaterInstance::startUpdate);
+        this.addDrawableChild(buttonWidget(this.width / 2 + 15, this.height / 2 + 50, 120, 20, VersionedText.translatable("automodpack.danger.highendconfirm").formatted(Formatting.BOLD), button -> {
+            Util.getMainWorkerExecutor().execute(modpackUpdaterInstance::startHighUpdate);
+        }));
+
+        this.addDrawableChild(buttonWidget(this.width / 2 + 15, this.height / 2 + 50, 120, 20, VersionedText.translatable("automodpack.danger.lowendconfirm").formatted(Formatting.BOLD), button -> {
+            Util.getMainWorkerExecutor().execute(modpackUpdaterInstance::startLowUpdate);
+        }));
+        
+        this.addDrawableChild(buttonWidget(this.width / 2 + 15, this.height / 2 + 50, 120, 20, VersionedText.translatable("automodpack.danger.completeconfirm").formatted(Formatting.BOLD), button -> {
+            Util.getMainWorkerExecutor().execute(modpackUpdaterInstance::startServerUpdate);
         }));
     }
 
