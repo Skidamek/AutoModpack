ed818ffcb5befee507abc538a50cc6360baa2427
skidam <skidamdev@proton.me>
2025-05-07 11:45:34 +0200

    bump zstd and httpclient5

--- loader/loader-fabric-core.gradle.kts
+++ loader/loader-fabric-core.gradle.kts
@@ -24,10 +24,11 @@ dependencies {
 
     compileOnly("com.google.code.gson:gson:2.10.1")
     compileOnly("org.apache.logging.log4j:log4j-core:2.20.0")
+
     implementation("org.tomlj:tomlj:1.1.1")
     implementation("org.bouncycastle:bcpkix-jdk18on:1.80")
-    implementation("com.github.luben:zstd-jni:1.5.7-1")
-    implementation("org.apache.httpcomponents.client5:httpclient5:5.4.3")
+    implementation("com.github.luben:zstd-jni:1.5.7-3")
+    implementation("org.apache.httpcomponents.client5:httpclient5:5.4.4")
 
     compileOnly("net.fabricmc:fabric-loader:${property("loader_fabric")}")
 }
