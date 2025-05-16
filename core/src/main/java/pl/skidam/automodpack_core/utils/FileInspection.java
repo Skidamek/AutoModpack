292c062f77bad556704d98087732438221fbe0f3
skidam <skidamdev@proton.me>
2025-05-16 14:08:48 +0200

    Add neo/forge environment detection logic to FileInspection.java ref #370

--- core/src/main/java/pl/skidam/automodpack_core/utils/FileInspection.java
+++ core/src/main/java/pl/skidam/automodpack_core/utils/FileInspection.java
@@ -347,8 +347,26 @@ public class FileInspection {
                     }
                     return dependencies;
                 }
-                case "environment" -> { // There's no way to check that on neo/forge
-                    return null;
+                case "environment" -> {
+                    LoaderManagerService.EnvironmentType environment = LoaderManagerService.EnvironmentType.UNIVERSAL;
+                    String modID = getModID(file);
+                    TomlArray dependenciesArray = result.getArray("dependencies.\"" + modID + "\"");
+                    if (dependenciesArray == null) {
+                        return environment;
+                    }
+
+                    for (Object o : dependenciesArray.toList()) {
+                        TomlTable mod = (TomlTable) o;
+                        if (mod == null) continue;
+                        String depEnv = mod.getString("side");
+                        if (depEnv == null) continue;
+                        switch (depEnv.toLowerCase()) {
+                            case "client" -> environment = LoaderManagerService.EnvironmentType.CLIENT;
+                            case "server" -> environment = LoaderManagerService.EnvironmentType.SERVER;
+                        }
+                    }
+
+                    return environment;
                 }
             }
         } catch (Exception e) {
@@ -410,14 +428,16 @@ public class FileInspection {
             case "environment" -> {
                 if (json.has("environment")) {
                     String environment = json.get("environment").getAsString();
-                    return switch (environment) {
+                    if (environment == null) return LoaderManagerService.EnvironmentType.UNIVERSAL;
+                    return switch (environment.toLowerCase()) {
                         case "client" -> LoaderManagerService.EnvironmentType.CLIENT;
                         case "server" -> LoaderManagerService.EnvironmentType.SERVER;
                         default -> LoaderManagerService.EnvironmentType.UNIVERSAL;
                     };
                 } else if (json.has("quilt_loader") && json.get("minecraft").getAsJsonObject().has("environment")) {
                     String environment = json.get("minecraft").getAsJsonObject().get("environment").getAsString();
-                    return switch (environment) {
+                    if (environment == null) return LoaderManagerService.EnvironmentType.UNIVERSAL;
+                    return switch (environment.toLowerCase()) {
                         case "client" -> LoaderManagerService.EnvironmentType.CLIENT;
                         case "server" -> LoaderManagerService.EnvironmentType.SERVER;
                         default -> LoaderManagerService.EnvironmentType.UNIVERSAL;
