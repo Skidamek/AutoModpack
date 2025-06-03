fbd86836fd21d923f69f47497b46a16e566334cd
Skidam <67871298+Skidamek@users.noreply.github.com>
2025-06-03 18:09:04 +0200

    spelling
    Co-authored-by: duckymirror <33291860+duckymirror@users.noreply.github.com>

--- core/src/main/java/pl/skidam/automodpack_core/protocol/netty/handler/ProtocolServerHandler.java
+++ core/src/main/java/pl/skidam/automodpack_core/protocol/netty/handler/ProtocolServerHandler.java
@@ -42,7 +42,7 @@ public class ProtocolServerHandler extends ByteToMessageDecoder {
             handlers.forEach((name, handler) -> ctx.pipeline().remove(handler));
 
             setupPipeline(ctx);
-        } else if (sslCtx == null || serverConfig.bindPort != -1) { // However if theres no magic packet and we dont use internal TLS or we are hosting on separate port, we have to try to connect anyway, for use with reverse proxy setups
+        } else if (sslCtx == null || serverConfig.bindPort != -1) { // However if there's no magic packet and we don't use internal TLS or we are hosting on a separate port, we have to try to connect anyway, for use with reverse proxy setups
             setupPipeline(ctx);
         }
 
