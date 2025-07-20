9de73d9e7bd662f4cf62ef9f7a76ec00df864f49
skidam <skidamdev@proton.me>
2025-07-21 00:11:30 +0200

    Refactor isPlayerAuthorized to use server thread

--- src/main/java/pl/skidam/automodpack/modpack/GameHelpers.java
+++ src/main/java/pl/skidam/automodpack/modpack/GameHelpers.java
@@ -3,8 +3,9 @@ package pl.skidam.automodpack.modpack;
 import com.mojang.authlib.GameProfile;
 import java.net.SocketAddress;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicBoolean;
+
 import net.minecraft.server.players.GameProfileCache;
-import net.minecraft.server.players.PlayerList;
 
 import static pl.skidam.automodpack.init.Common.server;
 
@@ -12,18 +13,23 @@ public class GameHelpers {
 
     // Simpler version of `PlayerManager.checkCanJoin`
     public static boolean isPlayerAuthorized(SocketAddress address, GameProfile profile) {
-        var playerManager = server.getPlayerList();
-        if (playerManager.getBans().isBanned(profile)) {
-            return false;
-        }
-        if (!playerManager.isWhiteListed(profile)) {
-            return false;
-        }
-        if (playerManager.getIpBans().isBanned(address)) {
-            return false;
-        }
+        AtomicBoolean isAuthorized = new AtomicBoolean(false);
+        server.submit(() -> {
+            var playerManager = server.getPlayerList();
+            if (playerManager.getBans().isBanned(profile)) {
+                return;
+            }
+            if (!playerManager.isWhiteListed(profile)) {
+                return;
+            }
+            if (playerManager.getIpBans().isBanned(address)) {
+                return;
+            }
 
-        return true;
+            isAuthorized.set(true);
+        }).join();
+
+        return isAuthorized.get();
     }
 
     // Method to get GameProfile from UUID with accounting for a fact that this player may not be on the server right now
