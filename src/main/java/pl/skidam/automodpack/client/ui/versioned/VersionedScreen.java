4bcc0f35aaa616b5c8f5852499e38216ccedbae1
skidam <skidamdev@proton.me>
2025-10-05 07:33:39 +0200

    fix compile

--- src/main/java/pl/skidam/automodpack/client/ui/versioned/VersionedScreen.java
+++ src/main/java/pl/skidam/automodpack/client/ui/versioned/VersionedScreen.java
@@ -6,7 +6,7 @@ import net.minecraft.client.gui.components.Button;
 import net.minecraft.client.gui.components.SpriteIconButton;
 /*?} else {*/
 /*import net.minecraft.client.gui.components.ImageButton;
-/*?}*/
+*//*?}*/
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
