751fda2d46f791d45948ef1be4ff9de25e6bfc3b
skidam <skidamdev@proton.me>
2025-12-31 16:20:22 +0100

    update stonecutter and moddev gradle plugins use regex for `ResourceLocation` <-> `Identifier` replacement

--- stonecutter.gradle.kts
+++ stonecutter.gradle.kts
@@ -2,9 +2,9 @@ plugins {
     id("dev.kikugie.stonecutter")
     kotlin("jvm") version "2.3.0" apply false
     id("fabric-loom") version "1.14-SNAPSHOT" apply false
-    id("net.neoforged.moddev") version "2.0.126" apply false
+    id("net.neoforged.moddev") version "2.0.136" apply false
     id("com.gradleup.shadow") version "9.3.0" apply false
-    id("org.moddedmc.wiki.toolkit") version "0.3.2"
+    id("org.moddedmc.wiki.toolkit") version "0.4+"
 }
 
 wiki {
@@ -19,48 +19,17 @@ stonecutter.parameters {
     constants.match(node.metadata.project.substringAfterLast('-'), "fabric", "neoforge", "forge")
 
     replacements {
-        string {
-            direction = current.parsed >= "1.20.2"
+        string(current.parsed >= "1.20.2") {
             replace("ServerboundCustomQueryPacket", "ServerboundCustomQueryAnswerPacket")
-        }
-
-        string {
-            direction = current.parsed >= "1.20.2"
             replace(".SystemToastIds.", ".SystemToastId.")
         }
 
-        string {
-            direction = current.parsed >= "1.21.11"
-            replace("net.minecraft.resources.ResourceLocation", "net.minecraft.resources.Identifier")
+        regex(current.parsed >= "1.21.11") {
+            replace("\\bResourceLocation\\b" to "Identifier", "\\bIdentifier\\b" to "ResourceLocation")
         }
 
-        string {
-            direction = current.parsed >= "1.21.11"
-            replace(" ResourceLocation ", " Identifier ")
-        }
-
-        string {
-            direction = current.parsed >= "1.21.11"
-            replace("(ResourceLocation ", "(Identifier ")
-        }
-
-        string {
-            direction = current.parsed >= "1.21.11"
-            replace(" ResourceLocation>", " Identifier>")
-        }
-
-        string {
-            direction = current.parsed >= "1.21.11"
-            replace("<ResourceLocation,", "<Identifier,")
-        }
-
-        string {
-            direction = current.parsed >= "1.21.11"
+        string(current.parsed >= "1.21.11") {
             replace("net.minecraft.Util", "net.minecraft.util.Util")
-        }
-
-        string {
-            direction = current.parsed >= "1.21.11"
             replace("source.hasPermission(3))", "source.permissions().hasPermission(new Permission.HasCommandLevel(PermissionLevel.byId(3))))")
         }
     }
