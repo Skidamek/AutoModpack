cb7aacd83845e0f35db954a8c763a51cd6a440ee
skidam <skidamdev@proton.me>
2026-08-03 22:31:21 +0200

    Fix cross-platform cache test CI

--- core/src/test/java/pl/skidam/automodpack_core/utils/cache/FileMetadataCacheTest.java
+++ core/src/test/java/pl/skidam/automodpack_core/utils/cache/FileMetadataCacheTest.java
@@ -1,6 +1,7 @@
 package pl.skidam.automodpack_core.utils.cache;
 
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -19,12 +20,18 @@ class FileMetadataCacheTest {
 		Path file = temporaryDirectory.resolve("file.bin");
 		Files.writeString(file, "one");
 		FileTime firstTime = FileTime.from(Instant.ofEpochSecond(1_700_000_000L, 123_000_000L));
+		FileTime secondTime = FileTime.from(firstTime.toInstant().plusNanos(999_999L));
+		Files.setLastModifiedTime(file, firstTime);
+		FileTime storedFirstTime = Files.getLastModifiedTime(file);
+		Files.setLastModifiedTime(file, secondTime);
+		FileTime storedSecondTime = Files.getLastModifiedTime(file);
+		assumeTrue(!storedFirstTime.equals(storedSecondTime), "The filesystem does not expose sub-millisecond timestamps");
 		Files.setLastModifiedTime(file, firstTime);
 
 		try (FileMetadataCache cache = FileMetadataCache.open(temporaryDirectory.resolve("hash-cache.db"))) {
 			String firstHash = cache.getOrComputeHash(file);
 			Files.writeString(file, "two");
-			Files.setLastModifiedTime(file, FileTime.from(Instant.ofEpochSecond(1_700_000_000L, 123_000_001L)));
+			Files.setLastModifiedTime(file, secondTime);
 
 			assertNotEquals(firstHash, cache.getOrComputeHash(file));
 		}
