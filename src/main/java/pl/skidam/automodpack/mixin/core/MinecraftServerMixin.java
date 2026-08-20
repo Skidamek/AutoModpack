038a28bac5dac2263c7b9f9b624ca5b43db4d436
skidam <skidamdev@proton.me>
2026-08-20 10:57:58 +0200

    fix(mixins): require stable MixinExtras hooks

--- src/main/java/pl/skidam/automodpack/mixin/core/MinecraftServerMixin.java
+++ src/main/java/pl/skidam/automodpack/mixin/core/MinecraftServerMixin.java
@@ -2,8 +2,10 @@ package pl.skidam.automodpack.mixin.core;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
 import net.minecraft.server.MinecraftServer;
 
@@ -12,14 +14,17 @@ import pl.skidam.automodpack.init.Common;
 @Mixin(MinecraftServer.class)
 public class MinecraftServerMixin {
 
-	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;initServer()Z", shift = At.Shift.AFTER), method = "runServer")
-	private void afterSetupServer(CallbackInfo info) {
-		Common.server = (MinecraftServer) (Object) this;
+	@WrapOperation(method = "runServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/MinecraftServer;initServer()Z"))
+	private boolean afterSetupServer(MinecraftServer server, Operation<Boolean> original) {
+		boolean initialized = original.call(server);
+		Common.server = server;
 		Common.afterSetupServer();
+		return initialized;
 	}
 
-	@Inject(at = @At("HEAD"), method = "stopServer")
-	private void beforeShutdownServer(CallbackInfo info) {
+	@WrapMethod(method = "stopServer")
+	private void beforeShutdownServer(Operation<Void> original) {
 		Common.beforeShutdownServer();
+		original.call();
 	}
 }
