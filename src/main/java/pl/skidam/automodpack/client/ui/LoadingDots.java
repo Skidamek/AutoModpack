fdebb68f7422479353741289457df18e8dcce6fa
skidam <skidamdev@proton.me>
2026-08-21 11:55:45 +0200

    Widen LoadingDots for cross-package screens

--- src/main/java/pl/skidam/automodpack/client/ui/LoadingDots.java
+++ src/main/java/pl/skidam/automodpack/client/ui/LoadingDots.java
@@ -3,13 +3,13 @@ package pl.skidam.automodpack.client.ui;
 import java.util.concurrent.TimeUnit;
 
 /** The four-frame loading marker used by vanilla multiplayer screens. */
-final class LoadingDots {
+public final class LoadingDots {
 	private static final String[] FRAMES = {"O o o", "o O o", "o o O", "o O o"};
 	private static final long FRAME_NANOS = TimeUnit.MILLISECONDS.toNanos(300);
 
 	private LoadingDots() {}
 
-	static String frame(long elapsedNanos) {
+	public static String frame(long elapsedNanos) {
 		return FRAMES[(int) (Math.max(0, elapsedNanos) / FRAME_NANOS % FRAMES.length)];
 	}
 }
