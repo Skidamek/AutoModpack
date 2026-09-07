c81fcb2b37284c6ada3104fbaf0ae9a9fec0b37e
skidam <skidamdev@proton.me>
2026-09-07 20:07:16 +0200

    Lay dialog bodies out with their action rows as one centered block

--- src/main/java/pl/skidam/automodpack/client/ui/UiFormat.java
+++ src/main/java/pl/skidam/automodpack/client/ui/UiFormat.java
@@ -14,7 +14,6 @@ import net.minecraft.locale.Language;
 import net.minecraft.network.chat.MutableComponent;
 
 import pl.skidam.automodpack.client.ui.versioned.VersionedText;
-import pl.skidam.automodpack_core.update.UpdatePlan;
 import pl.skidam.automodpack_core.utils.ByteFormat;
 import pl.skidam.automodpack_core.utils.PluralCategory;
 
@@ -52,21 +51,5 @@ public final class UiFormat {
 
 	public static String formatInstant(Instant instant) {
 		return HISTORY_TIME.format(instant);
-	}
-
-	static String filePath(UpdatePlan.FileKey file) {
-		return rootLabel(file.root()) + "/" + file.relativePath();
-	}
-
-	static String changePath(UpdatePlan.FileKey file) {
-		return file.relativePath();
-	}
-
-	static String rootLabel(UpdatePlan.Root root) {
-		return switch (root) {
-			case PROJECTION -> "active";
-			case OVERLAY -> "editable";
-			case GAME_DIR -> "game";
-		};
 	}
 }
